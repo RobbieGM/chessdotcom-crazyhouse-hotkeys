f2a08190848cf7c70e28f91f580b4a3c44a96004
RobbieGM <rgm020103@gmail.com>
2021-10-04 15:01:27 -0400

    feat: script for chess.com new interface (/play)

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Allows you to use the keyboard to highlight crazyhouse/bughouse piece drops.
 ## Installation
 1. Download TamperMonkey for your browser.
 2. Click the TamperMonkey extension, then click "Create a new script..."
-2. Copy [the contents of the main script file](https://raw.githubusercontent.com/RobbieGM/chessdotcom-crazyhouse-hotkeys/master/tampermonkey-script.js) into your new TamperMonkey script, and save it.
+2. Copy [the contents of the main script file](https://raw.githubusercontent.com/RobbieGM/chessdotcom-crazyhouse-hotkeys/master/tampermonkey-script-beta.js) into your new TamperMonkey script, and save it. If you are using chess.com's old interface (/live) then copy the contents of [this file](https://raw.githubusercontent.com/RobbieGM/chessdotcom-crazyhouse-hotkeys/master/tampermonkey-script.js) instead.
 3. Reload chess.com if it is open.
 
 ## Usage
---
+++ tampermonkey-script-beta.js
@@ -0,0 +1,71 @@
+// ==UserScript==
+// @name         Chess.com Crazyhouse Hotkeys
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  Use the keyboard to highlight crazyhouse/bughouse piece drops.
+// @author       Robbie Moore
+// @match        https://www.chess.com/game/live/*
+// @match        https://www.chess.com/play/online
+// @grant        none
+// ==/UserScript==
+
+const KEY_TO_PIECE_MAP = { // 5 6 7 8 9 free
+  a: "p",
+  s: "n",
+  d: "b",
+  f: "r",
+  g: "q",
+};
+
+let currentlySelectedPieceType = null;
+
+/**
+ * Selects a piece by its element and highlights it in the chess.com interface.
+ * @param {HTMLElement} piece
+ */
+function clickPieceElement(piece) {
+  piece.dispatchEvent(new MouseEvent("pointerdown", { bubbles: true }));
+  setTimeout(
+    () => piece.dispatchEvent(new MouseEvent("pointerup", { bubbles: true })),
+    1
+  );
+}
+
+function getUserColor() {
+  const isWhite = !document.querySelector('chess-board').classList.contains("flipped");
+  return isWhite ? "white" : "black";
+}
+
+function deselect() {
+  currentlySelectedPieceType = null;
+  document.body.dispatchEvent(new MouseEvent("pointerdown", { bubbles: true }));
+}
+
+async function selectPieceByType(type) {
+  if (currentlySelectedPieceType == type) {
+    deselect();
+    return;
+  }
+  const colorLetter = getUserColor() == "white" ? "w" : "b";
+  const pieceClass = colorLetter + type;
+  const element = document.querySelector(`div.hand-piece.${pieceClass}`);
+  if (element == null) {
+    return; // That piece doesn't show up because it's not in hand
+  }
+  if (currentlySelectedPieceType != null) {
+    // Deselect before selecting new piece to avoid glitches
+    deselect();
+    // await new Promise((res) => setTimeout(res, 100));
+  }
+  clickPieceElement(element);
+  currentlySelectedPieceType = type;
+}
+
+document.body.addEventListener("keydown", (event) => {
+  const isFocusedOnInput = document.activeElement instanceof HTMLInputElement;
+  if (!isFocusedOnInput && event.key in KEY_TO_PIECE_MAP) {
+    const piece = KEY_TO_PIECE_MAP[event.key];
+    selectPieceByType(piece);
+    event.stopImmediatePropagation(); // Prevent board emoji from showing up
+  }
+});
